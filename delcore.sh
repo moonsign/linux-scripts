ceedcec5b413dcd19676980577fe05c390ed331d
Dominic <moonsign@users.noreply.github.com>
2022-04-26 18:02:36 +0800

    fix(delcore): support crontab

--- delcore.sh
+++ delcore.sh
@@ -3,22 +3,22 @@ scriptname="$(basename $0)"
 prompt=" > "
 
 _usage() {
-    echo ""
-    echo "${scriptname}"
+    echo "${scriptname}:"
     echo "      Rename coredump file to core.[process]_[arg1]_[arg2]...[argn] and keep only the latest, periodically(per 10 min by default)."
     echo "      Default directories: user home, bin and fbin ."
-    echo "usage"
+    echo "usage:"
     echo "      ${scriptname} [interval] - to process every [interval] mins."
-    echo "      ${scriptname} [directory] - to process specified directory and exit."
-    echo "      ${scriptname} [-h/-help] - to show this message."
-    echo ""
+    echo "      ${scriptname} [dir1 dir2 ...] - to process specified directories only once."
+    echo "      ${scriptname} [-h|-help] - to show this message."
+    echo "note:"
+    echo "      If you want to use this with crontab, make sure to specify directories(the second usage above)."
 }
 
 _mvcore() {
     if [[ -d "$1" ]]; then
-        cd "$1"
-        echo "Renaming coredump files in \"${PWD}\"..."
-        count=$(ls core* 2>/dev/null | wc -l)
+        workdir="$(readlink -f "$1")"
+        echo "Renaming coredump files in \"${workdir}\"..."
+        count=$(ls "${workdir}"/core* 2>/dev/null | wc -l)
         if [[ ${count} -eq 0 ]]; then
             echo "${prompt}Great! No coredump file found\(^o^)/"
             echo 
@@ -26,21 +26,21 @@ _mvcore() {
         else
             echo "${prompt}${count} coredump file(s) found."
         fi
-        for corefile in $(ls -tr core*); do
-            ext="$(file -Pelf_phnum=10000 "${corefile}" | awk 'BEGIN{FS="from '\''|'\''"}{gsub("  *","_",$2);split($2,tmp,"/");print tmp[length(tmp)]}')"
-            newname="core.${ext}"
+        for corefile in $(ls -tr "${workdir}"/core*); do
+            ext="$(file -Pelf_phnum=10000 "${corefile}" | awk 'BEGIN{FS="from '\''|'\''"}{split($2,tmpa," ");gsub(/^.*\//,"",tmpa[1]);ext=tmpa[1];for(i=2;i<=length(tmpa);i++){ext=sprintf("%s_%s", ext,tmpa[i])};gsub(/[ \t.\/\\]/,"_",ext); print ext}'|\tr -s '_')"
+            newname="${workdir}/core.${ext}"
             if [[ "${corefile}" == "${newname}" ]]; then
-                echo "${prompt}Skipping \"${corefile}\""
+                echo "${prompt}Skipping \"$(basename ${corefile})\""
             else
-                echo "${prompt}Renaming \"${corefile}\"${prompt}\"${newname}\""
+                echo "${prompt}Renaming \"$(basename ${corefile})\" -> \"$(basename ${newname})\""
                 cmd="mv -f \"${corefile}\" \"${newname}\""
-                eval "${cmd}" 2>/dev/null
+                # echo "${cmd}"
+                eval "${cmd}"
             fi
         done
         echo ""
     else
         echo "$1: No such directory."
-        _usage
         return 1
     fi
 }
@@ -67,7 +67,9 @@ if [[ $1 ]]; then
         _usage
         exit 0
     else
-        _mvcore $1
+        for _dir in "$@"; do
+            _mvcore "${_dir}"
+        done
         exit 0
     fi
 else
